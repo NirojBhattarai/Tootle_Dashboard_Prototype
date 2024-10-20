a45f4982263d92c6201546768a20ab76d0cfaaf7
NirojBhattarai <neerajbhttri@gmail.com>
2024-10-20 12:06:31 +0545

    Update Database Connection String

--- backend_tootle/db.js
+++ backend_tootle/db.js
@@ -6,6 +6,12 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 3306,
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0,
+  acquireTimeout: 10000, // 10 seconds
+  connectTimeout: 10000, // 10 seconds
 });
 
 module.exports = pool;
